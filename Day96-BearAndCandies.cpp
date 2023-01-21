f0d9ea6677d4e4a4925102541983e8fd982565a4
Deepbhalsod <92981386+Deepbhalsod@users.noreply.github.com>
2023-01-21 19:58:15 +0530

    Add files via upload

---
+++ Day96-BearAndCandies.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+
+    string bearAndCandies(int limak, int bob)
+    {
+        int sumLimak = 0, sumBob = 0;
+        int i = 1;
+        
+        while(true)
+        {
+            if(i%2==0)
+                sumBob += i;
+            else
+                sumLimak += i;
+            
+            if(sumLimak > limak)
+                return "Bob";
+            if(sumBob > bob)
+                return "Limak";
+
+            i++;
+        }
+    }
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a;
+        int b;
+        cin>>a;
+        cin>>b;
+
+        Solution ob;    
+        cout <<ob.bearAndCandies(a, b)<<endl;
+    }
+    return 0;
+}
